c819bd64fe3bd33aa554d5ef7c298241e8a07141
Jansson Kaj <bgz800@myy.haaga-helia.fi>
2023-09-07 08:38:38 +0300

    first commit

---
+++ janssonbookstore/src/main/java/com/example/janssonbookstore/JanssonbookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.janssonbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanssonbookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanssonbookstoreApplication.class, args);
+	}
+
+}
---
+++ janssonbookstore/src/test/java/com/example/janssonbookstore/JanssonbookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.janssonbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JanssonbookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
