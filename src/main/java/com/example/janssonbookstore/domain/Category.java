5b6a3a3608cccd5a0b55ce1ca528bebf723594e8
Kaj Jansson <kaj.jansson@myy.haaga-helia.fi>
2023-09-21 11:31:36 +0300

    Sixth commit, added REST and API functions.

--- src/main/java/com/example/janssonbookstore/domain/Category.java
+++ src/main/java/com/example/janssonbookstore/domain/Category.java
@@ -2,6 +2,8 @@ package com.example.janssonbookstore.domain;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -16,7 +18,9 @@ public class Category {
 	private long categoryId;
 	private String categoryName;
 
-	// Database part 2, One-to-many relation to Category.java
+	// REST and database, One-to-many relation to Category.java
+	// with IgnoreJSON annotation to stop looping
+	@JsonIgnore
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
 	private List<Book> books;
 
